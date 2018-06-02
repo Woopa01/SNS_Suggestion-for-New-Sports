4e2ff2f8d4984206556753a95e58c5cca0b33e58
Woopa01 <whdnwls49@naver.com>
2018-06-02 18:31:53 +0900

    first commit

---
+++ app/src/androidTest/java/dsm2017/com/sns/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package dsm2017.com.sns
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("dsm2017.com.sns", appContext.packageName)
+    }
+}
