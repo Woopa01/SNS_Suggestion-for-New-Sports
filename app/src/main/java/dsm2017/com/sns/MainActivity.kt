55591c93214d71f94869b7a2705843cb878a6185
Woopa01 <whdnwls49@naver.com>
2018-06-02 20:28:39 +0900

    add splash

--- app/src/main/java/dsm2017/com/sns/MainActivity.kt
+++ app/src/main/java/dsm2017/com/sns/MainActivity.kt
@@ -1,12 +1,21 @@
 package dsm2017.com.sns
 
+import android.app.ActionBar
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.text.Layout
+import android.text.style.BackgroundColorSpan
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
---
+++ app/src/main/java/dsm2017/com/sns/SplashActivity.kt
@@ -0,0 +1,17 @@
+package dsm2017.com.sns
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Thread.sleep(2000)
+
+        val intent = Intent(SplashActivity@this,MainActivity::class.java)
+        startActivity(intent)
+    }
+}
