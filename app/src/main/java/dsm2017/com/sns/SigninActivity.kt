f946b7a7fbd8d3e406fbd8c0ecf6d044a81198c4
Woopa01 <whdnwls49@naver.com>
2018-06-02 23:08:02 +0900

    add signup,list,feedback

--- app/src/main/java/dsm2017/com/sns/SigninActivity.kt
+++ app/src/main/java/dsm2017/com/sns/SigninActivity.kt
@@ -1,12 +1,19 @@
 package dsm2017.com.sns
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_signin.*
 
 class SigninActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
+
+        signin_signup_button.setOnClickListener {
+            val intent = Intent(SigninActivity@this,SignupActivity::class.java)
+            startActivity(intent)
+        }
 
 
     }
